ecca8c92c229f0e0a9c18969e284f8a227332206
Hruthik B <hruthikbala2004@gmail.com>
2025-08-12 23:07:45 +0530

    Create 2787. Ways to Express an Integer as Sum of Powers.py

---
+++ 2787. Ways to Express an Integer as Sum of Powers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        MOD = 10**9 + 7
+        powers = []
+        i = 1
+        while (p := i ** x) <= n:
+            powers.append(p)
+            i += 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for p in powers:
+            for s in range(n, p - 1, -1):
+                dp[s] = (dp[s] + dp[s - p]) % MOD
+        return dp[n]
